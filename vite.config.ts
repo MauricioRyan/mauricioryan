8d29613699e1392a42635010586121ef120efacc
Mauricio Ryan <mauricioryan@gmail.com>
2026-02-05 12:15:30 -0300

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -6,7 +6,7 @@ import { componentTagger } from "lovable-tagger";
 // https://vitejs.dev/config/
 export default defineConfig(({ mode }) => ({
   // AGREGÁ ESTA LÍNEA:
-  base: "/", 
+  base: "./", 
   
   server: {
     host: "::",
